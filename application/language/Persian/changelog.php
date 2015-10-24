3d93230eb36ea00fd4d1d6c1ad444e5dbdfd101a
Nightprince <keramatjokar8855@gmail.com>
2015-10-24 16:01:12 +0330

    2 Language Change log

--- application/language/Persian/changelog.php
+++ application/language/Persian/changelog.php
@@ -1,22 +1,17 @@
 <?php
 
-/**
- * Note to module developers:
- * 	Keeping a module specific language file like this
- *	in this external folder is not a good practise for
- *	portability - I do not advice you to do this for
- *	your own modules since they are non-default.
- *	Instead, simply put your language files in
- *	application/modules/yourModule/language/
- *	You do not need to change any code, the system
- *	will automatically look in that folder too.
- */
-
 $lang['changelog_title'] = "تغییرات";
 $lang['changes_made_on'] = "حالت تغییرات روشن شد";
 $lang['no_changes'] = "تغییراتی جهت نمایش وجود ندارد!";
 $lang['change_info'] = "چه پروژه ای به اتمام رسید؟";
 $lang['category_name'] = "نام دسته";
 $lang['new_change'] = "نام پروژه تغییر یافته";
-$lang['new_category'] = "دسته جدید";
+$lang['new_category'] = "ایجاد دسته جدید";
+$lang['new_category2'] = " &rarr; ایجاد دسته جدید";
 $lang['add'] = "اضافه";
+$lang['edit'] = "ویرایش";
+$lang['pm'] = "پیام";
+$lang['save'] = "ذخیره";
+$lang['update'] = "آپدیت ها";
+$lang['c_n_c'] = "تغییر نام دسته";
+$lang['r_c_u'] = "حذف دسته و کلیه آپدیت های این دسته";
